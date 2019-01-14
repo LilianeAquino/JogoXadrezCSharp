17c39fbb2d6997c394dacd56b8237a3caea78a37
Liliane Aquino <lilianelou@hotmail.com>
2019-01-14 18:21:03 -0200

    Implementada a classe Posicao

---
+++ JogoXadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿namespace Tabuleiro
+{
+    //Em qual linha e coluna uma peça está
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + "," + Coluna;
+        }
+    }
+}
